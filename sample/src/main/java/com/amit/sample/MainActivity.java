48b0027e463c5f23aeedd87dd60b8ba2db344f9c
Ramchandra Singh <rcsingh4@gmail.com>
2018-05-19 16:43:50 +0530

    1. Added new libraries.

--- sample/src/main/java/com/amit/sample/MainActivity.java
+++ sample/src/main/java/com/amit/sample/MainActivity.java
@@ -1,26 +1,23 @@
 package com.amit.sample;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
-import com.amit.animation.AnimUtil;
-import com.amit.ui.CircularProgressButton;
-import com.amit.ui.ProgressBtn;
-import com.amit.ui.ToastMsg;
-import com.amit.ui.Toggle;
-import com.amit.ui.ToggleButtonLayout;
+/*import com.uniquestudio.library.CircleCheckBox;*/
+
+/*import com.github.angads25.toggle.LabeledSwitch;
+import com.github.angads25.toggle.interfaces.OnToggledListener;*/
+
+/*import com.fenjuly.library.ArrowDownloadButton;*/
+/*import com.sackcentury.shinebuttonlib.ShineButton;*/
 
 public class MainActivity extends AppCompatActivity
 {
-    ProgressBtn progressBtn;
-    ToggleButtonLayout toggleButton;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private int count = 0;
+    private int progress = 0;
+    /*private ArrowDownloadButton downloadButton;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,59 +25,115 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBtn = findViewById(R.id.progressBtn);
-
-        progressBtn.setOnClickListener(new View.OnClickListener()
+        /**
+         * Circle check box
+         *
+         * https://github.com/CoXier/CheckBox?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=4357
+         *
+         * implementation 'com.uniquestudio:checkbox:1.0.10'
+         *
+         * */
+        /*CircleCheckBox checkBox = findViewById(R.id.circle_check_box);
+        checkBox.setListener(new CircleCheckBox.OnCheckedChangeListener()
         {
             @Override
-            public void onClick(View v)
+            public void onCheckedChanged(boolean isChecked)
             {
-                progressBtn.startAnimation();
-                Handler handler = new Handler();
-
-                handler.postDelayed(new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        Drawable drawable = getResources().getDrawable(R.drawable.ic_check_white_48dp);
-                        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-                        progressBtn.doneLoadingAnimation(getResources().getColor(R.color.green), bitmap);
-                        // progressBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_green_bg));
-
-                        /*progressBtn.revertAnimation(new OnAnimEndListener()
-                        {
-                            @Override
-                            public void onAnimationEnd()
-                            {
-                                progressBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_green_bg));
-                            }
-                        });*/
-                    }
-                }, 2000);
-
-                /*handler.postDelayed(new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        progressBtn.revertAnimation();
-                    }
-                }, 3000);*/
+                // do something
             }
-        });
+        });*/
 
-        final Button bounceButton = findViewById(R.id.btnBounce);
-        bounceButton.setOnClickListener(new View.OnClickListener()
+        /**
+         * labeled switch
+         *
+         * https://github.com/Angads25/android-toggle?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=6778
+         *
+         * implementation 'com.github.angads25:toggle:1.0.0'
+         *
+         * */
+        /*LabeledSwitch labeledSwitch = findViewById(R.id.switchBtn);
+        labeledSwitch.setEnabled(false);*/
+        /*labeledSwitch.setOnToggledListener(new OnToggledListener()
+        {
+            @Override
+            public void onSwitched(LabeledSwitch labeledSwitch, boolean isOn)
+            {
+                // Implement your switching logic here
+            }
+        });*/
+
+        /**
+         *
+         * Download button with arrow
+         *
+         * https://github.com/fenjuly/ArrowDownloadButton?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=2307
+         *
+         * implementation 'com.github.fenjuly:ArrowDownloadButton:9e15b85e8a'
+         *
+         * */
+        /*downloadButton = findViewById(R.id.arrow_download_button);
+        downloadButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                AnimUtil.bounceAnim(MainActivity.this, bounceButton);
+                if ((count % 2) == 0)
+                {
+                    downloadButton.startAnimating();
+                    Timer timer = new Timer();
 
-                /*
-                Simple Dialog Dependency
-                implementation 'com.brouding:android-simple-dialog:0.3.1'
+                    timer.schedule(new TimerTask()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            runOnUiThread(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    progress = progress + 1;
+                                    downloadButton.setProgress(progress);
+                                }
+                            });
+                        }
+                    }, 2000, 20);
+                }
+                else
+                {
+                    downloadButton.reset();
+                }
+
+                count++;
+            }
+        });*/
+
+        /**
+         * spark button link
+         *
+         * implementation 'com.github.varunest:sparkbutton:1.0.5'
+         *
+         * https://android-arsenal.com/details/1/3876
+         *
+         * */
+
+        /**
+         * submit button link
+         * https://github.com/SparkYuan/SubmitButton
+         *
+         * repositories {
+         maven {
+         url 'https://dl.bintray.com/spark/maven'
+         }
+         }
+         *
+         *
+         * implementation 'me.spark:submitbutton:1.0.1'
+         * */
+
+        /*
+               Simple Dialog Dependency
+               implementation 'com.brouding:android-simple-dialog:0.3.1'
 
                 new SimpleDialog.Builder(MainActivity.this)
                         .setContent("This is a progress simple dialog.")
@@ -89,40 +142,43 @@ public class MainActivity extends AppCompatActivity
                         .setBtnCancelShowTime(5000)
                         .setBtnCancelTextColor(R.color.colorPrimary)
                         .show();*/
-            }
-        });
-
         /*
-        * implementation 'com.github.savvyapps:ToggleButtonLayout:latest.version.here'
-        * https://github.com/savvyapps/ToggleButtonLayout/tree/java
-        * */
-        toggleButton = findViewById(R.id.toggle);
-        toggleButton.OnToggleSelectedListener(new ToggleButtonLayout.OnToggledListener()
-        {
-            @Override
-            public void onToggled(Toggle toggle, boolean selected)
-            {
-                ToastMsg.info(MainActivity.this, "Selected toggle id is: " + toggle.getId()).show();
-            }
-        });
+         * implementation 'com.github.savvyapps:ToggleButtonLayout:latest.version.here'
+         * https://github.com/savvyapps/ToggleButtonLayout/tree/java
+         * */
 
         /**
          * loading button
          * https://github.com/rasoulmiri/ButtonLoading?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=6394
-        **/
+         **/
 
         /**
          * share button
          *
          * https://android-arsenal.com/details/1/4503
-        **/
+         **/
 
-        circularProgressButton = findViewById(R.id.circularButton3);
-        circularProgressButton.setIndeterminateProgressMode(true);
-        circularProgressButton.setStrokeColor(ContextCompat.getColor(this, R.color.colorAccent));
+        /**
+        * Shine button
+        *
+        * implementation 'com.sackcentury:shinebutton:0.2.0'
+        *
+        * https://github.com/ChadCSong/ShineButton?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=3846
+        *
+        * */
+        /*ShineButton shineButton = (ShineButton) findViewById(R.id.shine_button);
+        shineButton.init(MainActivity.this);*/
+
+        /**
+         *
+         * https://android-arsenal.com/details/1/3800
+         *
+         * https://android-arsenal.com/details/1/3799
+         *
+         * https://android-arsenal.com/tag/13?page=2&sort=created
+         *
+         * */
     }
-
-    CircularProgressButton circularProgressButton;
 
     /**
      * circular progress button
@@ -131,20 +187,5 @@ public class MainActivity extends AppCompatActivity
      *
      * implementation 'com.nihaskalam.android:progress-button:1.0.1'
      *
-    **/
-    public void progressButtonClick(View view)
-    {
-        if (circularProgressButton.isIdle())
-        {
-            circularProgressButton.showProgress();
-        }
-        else if (circularProgressButton.isErrorOrCompleteOrCancelled())
-        {
-            circularProgressButton.showIdle();
-        }
-        else if (circularProgressButton.isProgress())
-        {
-            circularProgressButton.showCancel();
-        }
-    }
+     **/
 }
